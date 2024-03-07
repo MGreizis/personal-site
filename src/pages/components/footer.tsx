d448439a71c4001d52f7c624428c2f58c418ed20
Martins Greizis <grei0003@hz.nl>
2024-03-07 13:02:04 -0700

    copyright year

--- src/pages/components/footer.tsx
+++ src/pages/components/footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="body-font bg-white text-cstmblack dark:bg-cstmpurple dark:text-beige">
       <div className="container mx-auto flex flex-col items-center justify-center px-5 py-8 sm:flex-row">
         <a className="title-font flex items-center justify-center font-medium text-cstmblack dark:text-beige md:justify-start">
-          <span className="text-l ml-3">&#169; Copyright 2023 MGreizis</span>
+          <span className="text-l ml-3">&#169; Copyright 2024 MGreizis</span>
         </a>
       </div>
     </footer>
