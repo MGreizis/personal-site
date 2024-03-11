0b55040d6cb44503a2bdb292e610e48bff7295c5
Martins Greizis <grei0003@hz.nl>
2024-03-11 15:00:58 -0600

    className renaming

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -13,6 +13,7 @@ const About: NextPage = () => {
   const descriptionTextClasses = "leading-relaxed text-white dark:text-cstmblack";
   const dateTextClasses = "text-sm font-bold tracking-widest mb-3 text-gray-500 uppercase tracking-wide";
   const boxClasses = "mx-auto mb-8 bg-cstmblack dark:bg-white rounded-xl shadow-md overflow-hidden md:max-w-[70%] md:min-w-[70%] sm:max-w-full";
+  const cityTextClasses = "text-white dark:text-cstmpurple";
 
   return (
     <>
@@ -38,7 +39,7 @@ const About: NextPage = () => {
             <div className="p-8 w-full">
               <h3 className={locationTextClasses}>HZ University of Applied Sciences</h3>
               <h2 className={experienceTextClasses}>Information and Communication Technologies</h2>
-              <p className={dateTextClasses}><span className="text-white dark:text-cstmpurple">Middelburg, NL</span> (2021-2025)</p>
+              <p className={dateTextClasses}><span className={cityTextClasses}>Middelburg, NL</span> (2021-2025)</p>
               <p className={descriptionTextClasses}>Pursuing my BSc in Software Engineering at HZ University of Applied Sciences in Middelburg, The Netherlands </p>
             </div>
           </div>
@@ -58,7 +59,7 @@ const About: NextPage = () => {
             <div className="p-8 w-full">
               <h3 className={locationTextClasses}>4Industry B.V</h3>
               <h2 className={experienceTextClasses}>Intern DevOps Engineer</h2>
-              <p className={dateTextClasses}><span className="text-white dark:text-cstmpurple">Utrecht, NL</span> (Sep 2024 - Jan 2025)</p>
+              <p className={dateTextClasses}><span className={cityTextClasses}>Utrecht, NL</span> (Sep 2024 - Jan 2025)</p>
               <p className={descriptionTextClasses}>Paid internship where my main goal was to increase traceability in the Software Development Lifecycle through the use of DevOps practices. This included improving existing CI/CD pipelines and creating entirely new ones</p>
             </div>
           </div>
@@ -77,7 +78,7 @@ const About: NextPage = () => {
             <div className="p-8 w-full">
               <h3 className={locationTextClasses}>Mount Royal University</h3>
               <h2 className={experienceTextClasses}>Exchange Program: Computer Science</h2>
-              <p className={dateTextClasses}><span className="text-white dark:text-cstmpurple">Calgary, CA</span> (Jan 2025 - May 2025)</p>
+              <p className={dateTextClasses}><span className={cityTextClasses}>Calgary, CA</span> (Jan 2025 - May 2025)</p>
               <p className={descriptionTextClasses}>Exchange semester in Canada, during which I followed the Computer Science courses, which allowed me to further improve my existing Web Development skills (React), as well as learn new skills in Data Structures (Java) and Systems Analysis</p>
             </div>
           </div>
