33f1ba24ee953a7f16033bcf95462405e3d0b15d
Martins Greizis <grei0003@hz.nl>
2024-05-17 14:45:15 +0300

    refactor projects into a component + add F1 project

---
+++ src/pages/components/projectsBox.tsx
@@ -0,0 +1,69 @@
+import Image, { type StaticImageData } from "next/image";
+import { motion } from "framer-motion";
+
+const ProjectsBox = ({ projectTitle, shortDescription, description, githubLink, image, viewProjectLink}: { 
+  projectTitle: string, 
+  shortDescription: string, 
+  description: string, 
+  githubLink: string, 
+  image: StaticImageData, 
+  viewProjectLink?: string 
+}) => {
+  const imageClasses = "mb-6 h-40 w-full rounded object-cover object-center";
+  const projectTitleClasses =
+    "text-xs font-bold tracking-widest text-white dark:text-cstmpurple";
+  const viewProjectLinkClasses =
+    "pt-2 pr-4 font-bold text-beige hover:text-white dark:text-cstmblack dark:hover:text-cstmpurple";
+  const shortDescriptionClasses =
+    "title-font mb-4 text-lg font-bold text-beige dark:text-cstmblack";
+  const descriptionClasses = "leading-relaxed text-white dark:text-cstmblack";
+
+  return (
+    <div className="p-4 md:w-1/2 xl:w-1/4">
+      <motion.div
+        className="rounded-lg bg-cstmblack p-6 dark:bg-white"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+        whileHover={{ scale: 1.1 }}
+      >
+        <Image
+          className={`${imageClasses}`}
+          src={image}
+          alt="content"
+          width="800"
+          height="500"
+        />
+        <h3 className={`${projectTitleClasses}`}>
+          {projectTitle}
+        </h3>
+        <h2 className={`${shortDescriptionClasses}`}>
+          {shortDescription}
+        </h2>
+        <p className={`${descriptionClasses}`}>
+          {description}
+        </p>
+        <div className="flex flex-wrap">
+          <a
+            className={`${viewProjectLinkClasses}`}
+            href={githubLink}
+            target="_blank"
+          >
+            GitHub
+          </a>
+          {viewProjectLink && (
+            <a
+              className={`${viewProjectLinkClasses}`}
+              href={viewProjectLink}
+              target="_blank"
+            >
+              Live Demo
+            </a>
+          )}
+        </div>
+      </motion.div>
+    </div>
+  );
+}
+
+export default ProjectsBox;
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -2,20 +2,14 @@ import { type NextPage } from "next";
 import Head from "next/head";
 import Header from "./components/header";
 import Footer from "./components/footer";
-import Image from "next/image";
-import { motion } from "framer-motion";
 import plushieMarketplace from "../../public/plushie-marketplace.png";
 import adventureGame from "../../public/adventure-game.png";
 import lwm from "../../public/lwm.png";
 import gym from "../../public/gym-site.png";
+import F1 from "../../public/F1.png";
+import ProjectsBox from "./components/projectsBox";
 
 const Projects: NextPage = () => {
-  const imageClasses = "mb-6 h-40 w-full rounded object-cover object-center";
-  const projectTitleClasses = "text-xs font-bold tracking-widest text-white dark:text-cstmpurple";
-  const viewProjectLinkClasses = "pt-2 pr-4 font-bold text-beige hover:text-white dark:text-cstmblack dark:hover:text-cstmpurple";
-  const shortDescriptionClasses = "title-font mb-4 text-lg font-bold text-beige dark:text-cstmblack";
-  const descriptionClasses = "leading-relaxed text-white dark:text-cstmblack";
-
   return (
     <>
       <Head>
@@ -31,164 +25,44 @@ const Projects: NextPage = () => {
       >
         <section className="body-font container mx-auto px-5 py-24">
           <div className="-m-4 flex flex-wrap">
-            <div className="p-4 md:w-1/2 xl:w-1/4">
-              <motion.div
-                className="rounded-lg bg-cstmblack p-6 dark:bg-white"
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ duration: 1 }}
-                whileHover={{ scale: 1.1 }}
-              >
-                <Image
-                  className={`${imageClasses}`}
-                  src={gym}
-                  alt="content"
-                  width="800"
-                  height="500"
-                />
-                <h3 className={`${projectTitleClasses}`}>
-                  GYM WEBSITE
-                </h3>
-                <h2 className={`${shortDescriptionClasses}`}>
-                  A simple gym website built with React
-                </h2>
-                <p className={`${descriptionClasses}`}>
-                  A gym website built with React and Tailwind CSS. It is
-                  fully responsive, yet simple and clean.
-                </p>
-                <div className="flex flex-wrap">
-                  <a
-                    className={`${viewProjectLinkClasses}`}
-                    href="https://github.com/MGreizis/gym-website"
-                    target="_blank"
-                  >
-                    GitHub
-                  </a>
-                  <a
-                    className={`${viewProjectLinkClasses}`}
-                    href="https://evogym-jade.vercel.app/"
-                    target="_blank"
-                  >
-                    Live Demo
-                  </a>
-                </div>
-              </motion.div>
-            </div>
-            <div className="p-4 md:w-1/2 xl:w-1/4">
-              <motion.div
-                className="rounded-lg bg-cstmblack p-6 dark:bg-white"
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ duration: 1 }}
-                whileHover={{ scale: 1.1 }}
-              >
-                <Image
-                  className={`${imageClasses}`}
-                  src={plushieMarketplace}
-                  alt="content"
-                  width="800"
-                  height="500"
-                />
-                <h3 className={`${projectTitleClasses}`}>
-                  MARKETPLACE
-                </h3>
-                <h2 className={`${shortDescriptionClasses}`}>
-                  Skeleton site for a Plushie Marketplace
-                </h2>
-                <p className={`${descriptionClasses}`}>
-                  A simple skeleton site for a plushie marketplace. It is built
-                  with plain HTML and CSS, as a group project.
-                </p>
-                <div className="flex flex-wrap">
-                  <a
-                    className={`${viewProjectLinkClasses}`}
-                    href="https://github.com/MGreizis/brainnest-week2"
-                    target="_blank"
-                  >
-                    GitHub
-                  </a>
-                  <a
-                    className={`${viewProjectLinkClasses}`}
-                    href="https://mgreizis.github.io/brainnest-week2/"
-                    target="_blank"
-                  >
-                    Live Demo
-                  </a>
-                </div>
-              </motion.div>
-            </div>
-            <div className="p-4 md:w-1/2 xl:w-1/4">
-              <motion.div
-                className="rounded-lg bg-cstmblack p-6 dark:bg-white"
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ duration: 1 }}
-                whileHover={{ scale: 1.1 }}
-              >
-                <Image
-                  className={`${imageClasses}`}
-                  src={adventureGame}
-                  alt="content"
-                  width="800"
-                  height="500"
-                />
-                <h3 className={`${projectTitleClasses}`}>
-                  CONSOLE GAME
-                </h3>
-                <h2 className={`${shortDescriptionClasses}`}>
-                  CLI Adventure Game built with Java
-                </h2>
-                <p className={`${descriptionClasses}`}>
-                  A simple CLI-based adventure game built with Java.
-                  Design Patterns were strictly followed to create the game.
-                </p>
-                <div className="flex flex-wrap">
-                  <a
-                    className={`${viewProjectLinkClasses}`}
-                    href="https://github.com/MGreizis/sde-console-program"
-                    target="_blank"
-                  >
-                    GitHub
-                  </a>
-                </div>
-              </motion.div>
-            </div>
-            <div className="p-4 md:w-1/2 xl:w-1/4">
-              <motion.div
-                className="rounded-lg bg-cstmblack p-6 dark:bg-white"
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ duration: 1 }}
-                whileHover={{ scale: 1.1 }}
-              >
-                <Image
-                  className={`${imageClasses}`}
-                  src={lwm}
-                  alt="content"
-                  width="800"
-                  height="500"
-                />
-                <h3 className={`${projectTitleClasses}`}>
-                  INTRANET
-                </h3>
-                <h2 className={`${shortDescriptionClasses}`}>
-                  An Intranet built with Laravel and Bulma
-                </h2>
-                <p className={`${descriptionClasses}`}>
-                  A group project for a company intranet using Laravel and 
-                  Bulma. Simple, yet functional and attractive.
-                </p>
-                <div className="flex flex-wrap">
-                  <a
-                    className={`${viewProjectLinkClasses}`}
-                    href="https://github.com/MGreizis/Team-10-LWM-Project"
-                    target="_blank"
-                  >
-                    GitHub
-                  </a>
-                </div>
-              </motion.div>
-            </div>
+            <ProjectsBox 
+              projectTitle={"F1 DATA VIEWER"}
+              shortDescription={"Dashboard for Formula 1 results"}
+              description={"Dashboard for viewing past Formula 1 results. Built with React, Material UI, Tailwind CSS, and Supabase"}
+              githubLink={"https://github.com/MGreizis/f1-data-dashboard"}
+              image={F1}
+              viewProjectLink={"https://f1-data-dashboard.vercel.app/"}
+            />
+            <ProjectsBox 
+              projectTitle={"GYM WEBSITE"}
+              shortDescription={"Simple gym website built with React"}
+              description={"Gym website built with React and Tailwind CSS. It is fully responsive, yet simple and clean."}
+              githubLink={"https://github.com/MGreizis/gym-website"}
+              image={gym}
+              viewProjectLink={"https://github.com/MGreizis/gym-website"}
+            />
+            <ProjectsBox 
+              projectTitle={"MARKETPLACE"}
+              shortDescription={"Skeleton site for a Plushie Marketplace"}
+              description={"Simple skeleton site for a plushie marketplace. It is built with plain HTML and CSS, as a group project."}
+              githubLink={"https://github.com/MGreizis/brainnest-week2"}
+              image={plushieMarketplace}
+              viewProjectLink={"https://mgreizis.github.io/brainnest-week2/"}
+            />
+            <ProjectsBox 
+              projectTitle={"CONSOLE GAME"}
+              shortDescription={"CLI Adventure Game built with Java"}
+              description={"Simple CLI-based adventure game built with Java. Design Patterns were strictly followed to create the game."}
+              githubLink={"https://github.com/MGreizis/sde-console-program"}
+              image={adventureGame}
+            />
+            <ProjectsBox 
+              projectTitle={"INTRANET"}
+              shortDescription={"An Intranet built with Laravel and Bulma"}
+              description={"Group project for a company intranet using Laravel and Bulma. Simple, yet functional and attractive."}
+              githubLink={"https://github.com/MGreizis/Team-10-LWM-Project"}
+              image={lwm}
+            />
           </div>
         </section>
       </main>
